91803fa7ea8d8af56d35d9280faf78b5d5fab6eb
acchavez89 <79331916+acchavez89@users.noreply.github.com>
2023-12-09 21:13:48 -0800

    Update symbolic_regress_mdl5.f90

--- aifeynman/symbolic_regress_mdl5.f90
+++ aifeynman/symbolic_regress_mdl5.f90
@@ -55,7 +55,7 @@
         real*8 epsilon, DL, nu, z
         real*8 lossbits, bitloss, meanbits, bestbits, bitmargin, bitsum, bitexcess, sigma, ev
 	parameter(epsilon=1/2.**30)
-	data arities /2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0/
+	data arities /2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0/
 	data functions /"+*-/><~\OJLESCANTRZ01P"/
       	integer nn(0:2), ii(nmax), kk(nmax), radix(nmax), iarr(nmax)
 	integer ndata, i, j, n
