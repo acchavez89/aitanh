e1bd11c93351d5054257b9515f076d88c7db9183
acchavez89 <79331916+acchavez89@users.noreply.github.com>
2023-12-09 21:22:07 -0800

    Update symbolic_regress1.f90

--- aifeynman/symbolic_regress1.f90
+++ aifeynman/symbolic_regress1.f90
@@ -44,7 +44,7 @@
 	parameter(nvarmax=20, nmax=10000000)
 	real*8 f, newloss, minloss, maxloss, rmsloss, xy(nvarmax+1,nmax), epsilon, DL, DL2, DL3
 	parameter(epsilon=0.00000001)
-	data arities /2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0/
+	data arities /2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0/
 	data functions /"+*-/><~\OJLESCANTRZ01P"/
       	integer nn(0:2), ii(nmax), kk(nmax), radix(nmax)
 	integer ndata, i, j, n
